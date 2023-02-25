f1d169123e00c626266caa0251cd18512b365d1d
Davi Holanda <daviholandas@gmail.com>
2023-02-25 13:59:44 -0300

    [FEAT] Create uses cases.

---
+++ src/RiseOn.RiseFinancial.Application/Configurations/PipelineBehaviors/ValidationPipelineBehavior.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.Result;
+using Mediator;
+
+namespace RiseOn.RiseFinancial.Application.Configurations.PipelineBehaviors;
+
+public class ValidationPipelineBehavior<TMessage,TResponse> 
+    : IPipelineBehavior<TMessage, TResponse>
+    where TMessage : IRequest
+    where TResponse : Result
+{
+    public ValueTask<TResponse> Handle(TMessage message, 
+        CancellationToken cancellationToken, 
+        MessageHandlerDelegate<TMessage, TResponse> next)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/RiseOn.RiseFinancial.Application/UseCases/Expense/CreateExpenseCommands.cs
@@ -0,0 +1,41 @@
+﻿using Ardalis.Result;
+using Mediator;
+using ExpenseAggregate = RiseOn.RiseFinancial.Core.ExpenseAggregate;
+
+namespace RiseOn.RiseFinancial.Application.UseCases.Expense;
+
+public record struct CreateFixedExpenseCommand(
+        decimal Value, string? Description,
+        string Recipient, string Category,
+        Guid WalletId, int Installment)
+    : ICommand<Result<Guid>>;
+
+public record struct CreateVariableExpenseCommand(
+        decimal Value, string? Description,
+        string Recipient, string Category,
+        Guid WalletId)
+    : ICommand<Result<Guid>>;
+
+
+public class CreateExpensesHandler : 
+    ICommandHandler<CreateFixedExpenseCommand, Result<Guid>>, 
+    ICommandHandler<CreateVariableExpenseCommand, Result<Guid>>
+{
+
+    public ValueTask<Result<Guid>> Handle(CreateFixedExpenseCommand command,
+        CancellationToken cancellationToken)
+    {
+        ExpenseAggregate.Expense expense = new(
+            command.Value, command.Description,
+            command.Recipient, command.Category,
+            command.WalletId, command.Installment);
+
+        return ValueTask.FromResult(Result<Guid>.Success(expense.Id));
+    }
+
+    public ValueTask<Result<Guid>> Handle(CreateVariableExpenseCommand command,
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
